00053ba67c3e5296e906a7bf926fa984cf5a79c8
Mengxi Zhang <zmx94824@gmail.com>
2020-12-02 12:08:51 -0800

    Add router

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Game Center</h1>
+        <h6>Please select a game to play:</h6>
+        <ul>
+          <li><Link to="/game">Single Player Game</Link></li>
+        </ul>
+      </div>
+    )
+  }
+}
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+
+import Home from './Home.js';
+import SinglePlayerUI from './SinglePlayerUI.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />,
-  </React.StrictMode>,
+  <Router history = {browserHistory}>
+      <Route path = "/">
+         <IndexRoute component = {Home} />
+         <Route path = "game" component = {SinglePlayerUI} />
+      </Route>
+   </Router>,
   document.getElementById('root')
 );
