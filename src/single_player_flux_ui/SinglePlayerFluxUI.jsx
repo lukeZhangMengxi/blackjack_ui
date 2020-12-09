cd1c33b76e11800ad084f1815a115e233b88aeb5
Mengxi Zhang <zmx94824@gmail.com>
2020-12-08 22:36:16 -0800

    Flux pattern signup dialog

--- src/single_player_flux_ui/SinglePlayerFluxUI.jsx
+++ src/single_player_flux_ui/SinglePlayerFluxUI.jsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { login, nav_signup, signup } from './actions/Actions.js'
+import { login, nav_signup, signup, nav_signup_cancel } from './actions/Actions.js'
 
 import LoginDialog from './components/LoginDialog'
+import SignupDialog from './components/SignupDialog'
 
 
 
 class SinglePlayerFluxUI extends Component {
   render() {
-    const { dispatch, stage } = this.props
+    const { dispatch, myState } = this.props
 
     return (
       <div>
         <LoginDialog
-          currentStage = {stage.stage}
-          onPlayerLoginClick = { (email, password) => dispatch(login(email, password)) }
-          onNavSignupClick = { () => dispatch(nav_signup()) }
+          currentStage={myState.stage}
+          onPlayerLoginClick={(playerId, jwt) => dispatch(login(playerId, jwt))}
+          onNavSignupClick={() => dispatch(nav_signup())}
+        />
+        <SignupDialog
+          currentStage={myState.stage}
+          onPlayerSignupClick={() => dispatch(signup())}
+          onCancelClick={() => dispatch(nav_signup_cancel())}
         />
       </div>
     )
@@ -24,7 +30,7 @@ class SinglePlayerFluxUI extends Component {
 
 function mapStateToProps(state) {
   return {
-    stage: state.nextStage
+    myState: state.nextStage
   }
 }
 
