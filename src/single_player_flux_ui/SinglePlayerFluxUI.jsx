3f4ed17bb5bdbfcaa8addb6b51eba37222de3b87
Mengxi Zhang <zmx94824@gmail.com>
2020-12-09 21:53:06 -0800

    Add UI

--- src/single_player_flux_ui/SinglePlayerFluxUI.jsx
+++ src/single_player_flux_ui/SinglePlayerFluxUI.jsx
@@ -4,6 +4,7 @@ import { login, nav_signup, signup, nav_signup_cancel } from './actions/Actions.
 
 import LoginDialog from './components/LoginDialog'
 import SignupDialog from './components/SignupDialog'
+import GameBoard from './components/GameBoard'
 
 
 
@@ -22,6 +23,13 @@ class SinglePlayerFluxUI extends Component {
           currentStage={myState.stage}
           onPlayerSignupClick={() => dispatch(signup())}
           onCancelClick={() => dispatch(nav_signup_cancel())}
+        />
+        <GameBoard
+          currentStage={myState.stage}
+          dealerCards={myState.dealerCards || []}
+          playerCards={myState.playerCards || []}
+          playerBalance={myState.playerBalance}
+          playerName={myState.playerName}
         />
       </div>
     )
---
+++ src/single_player_flux_ui/components/GameBoard.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+
+import { stages } from '../reducers/Reducers'
+import { getPokerImgName } from '../../PokerMapper'
+
+export default class GameBoard extends Component {
+
+  render() {
+    return (
+      <Container fluid>
+        <p>BlackJack UI, hello: {this.props.playerName}</p>
+        <Row style={{ position: "relative", top: 20 }}>
+          <Col sm={1}>Dealer's Cards: </Col>
+          {
+            this.props.dealerCards.map((element) => {
+              return (
+                <Col sm={1}>
+                  <Card style={{ width: 100 }}>
+                    <Card.Img src={'./img/' + getPokerImgName(element, '.png')} />
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
+        <Row style={{ position: "relative", top: 50 }}>
+          <Col >VS</Col>
+        </Row>
+        <Row style={{ position: "relative", top: 100 }}>
+          <Col sm={1}>Your Cards: </Col>
+          {
+            this.props.playerCards.map((element) => {
+              return (
+                <Col sm={1}>
+                  <Card style={{ width: 100 }}>
+                    <Card.Img src={'./img/' + getPokerImgName(element, '.png')} />
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
+        <Row style={{ position: "relative", top: 120 }}>
+          <Col>Current Balance: {this.props.playerBalance}</Col>
+        </Row>
+        <Row style={{ position: "relative", top: 150 }}>
+          {this.props.currentStage === stages.IDLE && <Col sm={2} offset={2}> <Button onClick={() => console.log('start')}>Start</Button> </Col>}
+          {this.props.currentStage === stages.PLAYER_TURN && <Col sm={2} offset={2}> <Button onClick={() => console.log('hit')}>Hit</Button> </Col>}
+          {this.props.currentStage === stages.PLAYER_TURN && <Col sm={2} offset={2}> <Button onClick={() => console.log('stand')}>Stand</Button> </Col>}
+          {this.props.currentStage === stages.IDLE && <Col sm={2} offset={2}> <Button onClick={() => console.log('signout')}>Sign out</Button> </Col>}
+        </Row>
+      </Container>
+    )
+  }
+
+}
